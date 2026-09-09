0bb49232a4d72eebb53cbfa0f3e085b47ba50eda
Vince Jason Haya <vyoshida02@gmail.com>
2026-09-09 09:53:09 +0800

    Remove comment from categories_delete.php
    Remove outdated comment regarding API usage.

--- api/categories_delete.php
+++ api/categories_delete.php
@@ -1,5 +1,5 @@
 <?php
-// api/categories_delete.php  (POST, must be logged in)  body: { "name": "..." }
+
 require_once __DIR__ . '/../config/helpers.php';
 requireLogin();
 require_once __DIR__ . '/../config/db.php';
